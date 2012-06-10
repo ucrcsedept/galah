a76e9b6aaa3ca127fb618923850ccc4e547495ad
John Sullivan <jsull003@ucr.edu>
2012-06-10 16:06:00 -0700

    n/a

--- galah-web/app/models/__init__.py
+++ galah-web/app/models/__init__.py
@@ -1,8 +1,14 @@
 import mongoengine
 mongoengine.connect("galah")
 
+# If were importing models from another component of galah the users shouldn't
+# (and can't) be loaded.
+try:
+    from users import User
+except ImportError:
+    pass
+
 from classes import Class
-from users import User
 from invitations import Invitation
 from assignments import Assignment
 from submissions import Submission
