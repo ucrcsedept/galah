cffcfb1e5cf250d30d62479fd4f971c46e99fc49
John Sullivan <jsull003@ucr.edu>
2012-06-10 16:08:06 -0700

    n/a

--- galah-web/app/models/__init__.py
+++ galah-web/app/models/__init__.py
@@ -5,10 +5,10 @@ mongoengine.connect("galah")
 # (and can't) be loaded.
 try:
     from users import User
+    from invitations import Invitation
 except ImportError:
     pass
 
 from classes import Class
-from invitations import Invitation
 from assignments import Assignment
 from submissions import Submission
