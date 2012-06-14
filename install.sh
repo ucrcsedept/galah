bf16f1d0af6a2136db125f76c96b69c7a7865a72
John Sullivan <jsull003@ucr.edu>
2012-06-13 18:11:50 -0700

    Added install script (not official install script, more a working set of notes) and made galah-db importable.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Temporary install script until a better one can be made. You can think of
+# this file in its current state to be a working set of notes.
+
+echo `pwd`/components > $1/galah.pth
