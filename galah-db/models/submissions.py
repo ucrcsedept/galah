20134ee39981e5b78ecee16c7aac41838a4d806e
John Sullivan <jsull003@ucr.edu>
2012-06-13 17:42:39 -0700

    Created galah-db directory to hold all database related files.
    I expect this directory to house all the db models of course, but also any scripts that perform magic on the database (for example, repair and diagnostic scripts). I do not expect scripts like add-user and such to exist here however, those belong elsewhere.

---
+++ galah-db/models/submissions.py
@@ -0,0 +1,49 @@
+from mongoengine import *
+
+def rigidDocument(zdocClass):
+    def validate(self, *zargs, **zkwargs):
+        for k, v in vars(self).items():
+            if k.startswith("_"): continue
+            
+            if k not in self._fields:
+                raise ValidationError("Extraneous attributes were found.")
+        
+        return super(zdocClass, self).validate(*zargs, **zkwargs)
+
+    zdocClass.validate = validate
+    
+    return zdocClass
+        
+@rigidDocument
+class SubTest(EmbeddedDocument):
+    name = StringField(required = True)
+    score = FloatField()
+    maxScore = FloatField()
+    message = StringField()
+    messages = ListField(StringField)
+
+    meta = {
+        "allow_inheritance": False
+    }
+
+@rigidDocument
+class TestResult(EmbeddedDocument):
+    subTests = ListField(EmbeddedDocumentField(SubTest))
+    score = FloatField()
+    maxScore = FloatField()
+    
+    meta = {
+        "allow_inheritance": False
+    }
+
+class Submission(Document):
+    assignment = ObjectIdField(required = True)
+    user = StringField(required = True)
+    timestamp = DateTimeField(required = True)
+    testables = StringField(required = True)
+    testResult = EmbeddedDocumentField(TestResult)
+    
+    meta = {
+        "allow_inheritance": False,
+        "ordering": ["-timestamp"]
+    }
