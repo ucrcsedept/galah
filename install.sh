27559764f07433684d62d15d9fc5c2899bf9f495
John Sullivan <jsull003@ucr.edu>
2012-06-17 00:14:30 -0700

    Fixed bug with install script

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 
 # Temporary install script until a better one can be made. You can think of
 # this file in its current state to be a working set of notes.
-
-echo `pwd`/components > $1/galah.pth
+ln -s `pwd`/components $1/galah
