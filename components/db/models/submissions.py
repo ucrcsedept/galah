d7fb67513ec6d4331cb113172fc31f7f99352ac9
John Sullivan <jsull003@ucr.edu>
2012-06-21 01:39:36 -0700

    Added support for uploading tar files.

--- components/db/models/submissions.py
+++ components/db/models/submissions.py
@@ -1,47 +1,47 @@
 from mongoengine import *
 
-def rigidDocument(zdocClass):
-    def validate(self, *zargs, **zkwargs):
-        for k, v in vars(self).items():
-            if k.startswith("_"): continue
+#def rigidDocument(zdocClass):
+    #def validate(self, *zargs, **zkwargs):
+        #for k, v in vars(self).items():
+            #if k.startswith("_"): continue
             
-            if k not in self._fields:
-                raise ValidationError("Extraneous attributes were found.")
+            #if k not in self._fields:
+                #raise ValidationError("Extraneous attributes were found.")
         
-        return super(zdocClass, self).validate(*zargs, **zkwargs)
+        #return super(zdocClass, self).validate(*zargs, **zkwargs)
 
-    zdocClass.validate = validate
+    #zdocClass.validate = validate
     
-    return zdocClass
+    #return zdocClass
         
-@rigidDocument
-class SubTest(EmbeddedDocument):
-    name = StringField(required = True)
-    score = FloatField()
-    max_score = FloatField()
-    message = StringField()
-    messages = ListField(StringField)
+#@rigidDocument
+#class SubTest(EmbeddedDocument):
+    #name = StringField(required = True)
+    #score = FloatField()
+    #max_score = FloatField()
+    #message = StringField()
+    #messages = ListField(StringField)
 
-    meta = {
-        "allow_inheritance": False
-    }
+    #meta = {
+        #"allow_inheritance": False
+    #}
 
-@rigidDocument
-class TestResult(EmbeddedDocument):
-    sub_tests = ListField(EmbeddedDocumentField(SubTest))
-    score = FloatField()
-    max_score = FloatField()
+#@rigidDocument
+#class TestResult(EmbeddedDocument):
+    #sub_tests = ListField(EmbeddedDocumentField(SubTest))
+    #score = FloatField()
+    #max_score = FloatField()
     
-    meta = {
-        "allow_inheritance": False
-    }
+    #meta = {
+        #"allow_inheritance": False
+    #}
 
 class Submission(Document):
     assignment = ObjectIdField(required = True)
     user = StringField(required = True)
     timestamp = DateTimeField(required = True)
     testables = StringField(required = True)
-    test_result = EmbeddedDocumentField(TestResult)
+    #test_result = EmbeddedDocumentField(TestResult)
     
     meta = {
         "allow_inheritance": False,
