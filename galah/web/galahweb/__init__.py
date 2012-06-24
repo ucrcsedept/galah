bbc470322a274a879abb95eaa6941e01f092686a
John Sullivan <jsull003@ucr.edu>
2012-06-24 12:45:52 -0700

    Added configuration options to galah.web.
    	* If the environmental variable GALAH_WEB_CONFIG is set it will be interpreted as a file path and configuration will be loaded from that file.
    	* If GALAH_WEB_CONFIG is not set galah.web will look for a file at /etc/galah/web.config and try to load the configuration frmo there.
    	* If neither exists **galah.web will not start up**!

--- galah/web/galahweb/__init__.py
+++ galah/web/galahweb/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app = Flask("galah.web.galahweb")
 
+if not app.debug:
+    if not app.config.from_envvar("GALAH_WEB_CONFIG", silent = True):
+        try:
+            app.config.from_pyfile("/etc/galah/web.config")
+        except IOError:
+            exit(
+                "Environmental variable GALAH_WEB_CONFIG not set to valid file "
+                "path and /etc/galah/web.config was not found. Either point "
+                "GALAH_WEB_CONFIG at a valid config file or place your config "
+                "file at /etc/galah/web.config."
+            )
+            
+            
+
 # Plug the auth system into our app
 from auth import login_manager
 login_manager.setup_app(app)
