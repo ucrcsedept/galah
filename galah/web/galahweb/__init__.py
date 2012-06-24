8bc2e991aaab7b6a3e80304b8119c1a613063f0a
John Sullivan <jsull003@ucr.edu>
2012-06-24 13:18:00 -0700

    Added ability to configure log handlers.
    * In the config file, just add a LOG_HANDLERS option that contains
    either a single handler or is a tuple of handlers. All handlers should
    derive from logging.Handler.

--- galah/web/galahweb/__init__.py
+++ galah/web/galahweb/__init__.py
@@ -1,18 +1,27 @@
 from flask import Flask
 app = Flask("galah.web.galahweb")
 
-if not app.debug:
-    if not app.config.from_envvar("GALAH_WEB_CONFIG", silent = True):
-        try:
-            app.config.from_pyfile("/etc/galah/web.config")
-        except IOError:
-            exit(
-                "Environmental variable GALAH_WEB_CONFIG not set to valid file "
-                "path and /etc/galah/web.config was not found. Either point "
-                "GALAH_WEB_CONFIG at a valid config file or place your config "
-                "file at /etc/galah/web.config."
-            )
-            
+if not app.config.from_envvar("GALAH_WEB_CONFIG", silent = True):
+    try:
+        app.config.from_pyfile("/etc/galah/web.config")
+    except IOError:
+        exit(
+            "Environmental variable GALAH_WEB_CONFIG not set to valid file "
+            "path and /etc/galah/web.config was not found. Either point "
+            "GALAH_WEB_CONFIG at a valid config file or place your config "
+            "file at /etc/galah/web.config."
+        )
+
+if "LOG_HANDLERS" in app.config:
+    import logging
+    
+    # If a single handler was passed directly, convert it into a one-tuple
+    if isinstance(app.config["LOG_HANDLERS"], logging.Handler):
+        app.config["LOG_HANDLERS"] = (app.config["LOG_HANDLERS"], )
+        
+    # Add any handlers the user wants
+    for i in app.config["LOG_HANDLERS"]:
+        app.logger.addHandler(i)
             
 
 # Plug the auth system into our app
