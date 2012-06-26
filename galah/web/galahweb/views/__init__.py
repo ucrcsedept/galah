a0aa1fb78e00cda72b11bf3675ff7559500e8564
John Sullivan <jsull003@ucr.edu>
2012-06-25 23:43:52 -0700

    Added nice error pages. With birds!
    * 404 and 500 (uncaught exceptions) errors will be caught and
      directed to a nice error page with some calming birds.

--- galah/web/galahweb/views/__init__.py
+++ galah/web/galahweb/views/__init__.py
@@ -3,3 +3,4 @@ from _browse_assignments import browse_assignments
 from _view_assignment import view_assignment
 from _upload_submission import upload_submission
 from _logout import logout
+from _error import error
---
+++ galah/web/galahweb/views/_error.py
@@ -0,0 +1,56 @@
+# The actual view
+from galah.web.galahweb import app
+from flask import render_template, url_for
+from collections import namedtuple
+
+AsciiPiece = namedtuple("AsciiPiece", ["author", "art"])
+ascii_art = {
+    500: AsciiPiece(
+        author = "pb", 
+        art = """     __     __------           
+  __/o `\ ,~   _~~  . ..   . ..
+ ~ -.   ,'   _~-----           
+     `\     ~~~--_'__          
+       `~-==-~~~~~---          """        
+    ),
+    404: AsciiPiece(
+        author = "Stephen Morgana",
+        art = """             /\\              
+              .\\\..          
+              \\   \\         
+              \ (o) /         
+              (/    \         
+               /\    \        
+              ///     \       
+             ///|     |       
+            ////|     |       
+           //////    /        
+           |////    /         
+          /|////--V/          
+         //\//|   |           
+     ___////__\___\__________ 
+    ()_________'___'_________)"""
+    )
+}
+
+messages = {
+    500: """It appears something broke, I'm going to go fix it... In the meantime <a href="%s">Try Here</a>.""",
+    404: """You seem to have landed in the wrong place... <a href="%s">Try Here</a>."""
+}
+
+@app.errorhandler(404)
+@app.errorhandler(500)
+def error(e):
+    code = e.code if hasattr(e, "code") else 500
+    
+    if code == 500:
+        error_description = "500: Internal Server Error"
+    else:
+        error_description = str(e)
+    
+    return render_template(
+        "error.html",
+        error_description = error_description,
+        message = messages[code] % "/home", 
+        art_piece = ascii_art[code]
+    ), code
