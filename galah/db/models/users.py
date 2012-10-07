64995b7d1df1adf473b12b9979fa73ab1da37220
John Sullivan <jsull003@ucr.edu>
2012-10-06 17:47:03 -0700

    Added a number of ue improvements to the API.
     * api_client.py may be executed as a normal executable.
     * The api client now checks for a configuration file and that
       can be used for authentication.
     * The instructor user type has been renamed to teacher as I was using
       teacher incorrectly in more places that I used instructor.
     * Output of API commands has been standardized. The user experience
       between each command should now be very alike.
     * find_user and find_class can now be easily used to list all users
       and classes respectively.
     * me may be specified whenever an email is required to use the
       currently-logged-in-user's email.

--- galah/db/models/users.py
+++ galah/db/models/users.py
@@ -3,7 +3,7 @@ from mongoengine import *
 class User(Document):
     email = EmailField(unique = True, primary_key = True)
     seal = StringField(required = True)
-    account_type = StringField(choices = ["student", "instructor", "admin"], required = True)
+    account_type = StringField(choices = ["student", "teacher", "admin"], required = True)
     classes = ListField(ObjectIdField())
     
     meta = {
