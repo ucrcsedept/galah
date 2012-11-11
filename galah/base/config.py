70be051463ef936cb17b2367267cd41646a9e0fe
John Sullivan <jsull003@ucr.edu>
2012-11-10 18:22:00 -0800

    Removed debugging code from config.

--- galah/base/config.py
+++ galah/base/config.py
@@ -1,21 +1,19 @@
 import imp
 
-def load_config(file, namespace):
-	loaded = imp.load_source("mouse", file)
+def load_config(file, domain):
+	loaded = imp.load_source("user_config_file", file)
 
 	local_config = {}
 
-	prefix = "%s/" % namespace
+	prefix = "%s/" % domain
 	global_prefix = "global/"
 
-	# Grab all the configuration values for the given namespace, and any global
+	# Grab all the configuration values for the given domain, and any global
 	# configuration values.
 	for k, v in loaded.config.items():
 		if k.startswith(prefix) and k != len(prefix):
 			local_config[k[len(prefix):]] = v
 		elif k.startswith(global_prefix) and k != len(global_prefix):
 			local_config[k[len(global_prefix):]] = v
-		else:
-			print k, v
 
 	return local_config
