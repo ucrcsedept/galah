0a6b41e0282b5e275e7ea8775057dbace9db9853
John Sullivan <jsull003@ucr.edu>
2012-11-13 15:38:53 -0800

    Fixed remaining issues with configuration.
    * Logging works correctly in all components.
    * Configuration uses defaults if it cannot find a configuration file.

--- galah/base/config.py
+++ galah/base/config.py
@@ -1,33 +1,90 @@
-import imp
-
 # Default configuration values for Galah.
 defaults = {
     "global/SUBMISSION_DIRECTORY": "/var/local/galah/web/submissions/",
     "global/MONGODB": "galah",
-	"web/DEBUG": True,
+    "global/HEAVYLIFTER_ADDRESS": "ipc:///tmp/heavylifter.sock",
+    "web/DEBUG": True,
     "web/SECRET_KEY": "Very Secure Key",
     "web/HOST_URL": "http://localhost:5000"
 }
 
-def load_config(file, domain):
-	loaded = imp.load_source("user_config_file", file)
+import imp
+import logging
+from galah.base.utility import tuplify
+from galah.base.magic import memoize
 
-	local_config = {}
+loaded = None
+@memoize
+def load_config(domain):
+    """
+    Parses and loads up the Galah configuration file and extracts the
+    configuration data for the given domain (ex: "web" or "sheep").
 
-	# Note we make a **shallow** copy of the defaults here
-	user_config = dict(defaults)
-	user_config.update(loaded.config)
+    Global configuration options will always be returned, no matter the domain.
 
-	# The prefix values we will look for when scanning the configuration file.
-	prefix = "%s/" % domain
-	global_prefix = "global/"
+    The configuration file will only be loaded once per instance of the
+    interpreter, so feel free to call this function multiple times in many files
+    to avoid ugly dependency issues.
 
-	# Grab all the configuration values for the given domain, and any global
-	# configuration values.
-	for k, v in user_config.items():
-		if k.startswith(prefix) and k != len(prefix):
-			local_config[k[len(prefix):]] = v
-		elif k.startswith(global_prefix) and k != len(global_prefix):
-			local_config[k[len(global_prefix):]] = v
+    """
 
-	return local_config
+    global loaded
+
+    # Note whether this is the first time we're processing the configuraiton. If
+    # it is, we'll want to attach the global log handlers.
+    first_load = loaded is None
+
+    # Load the configuration if it hasn't been loaded yet. Note this ensures
+    # that the configuration is only loaded once per instance of the
+    # interpreter
+    if not loaded:
+        try:
+            loaded = imp.load_source(
+                "user_config_file", "/etc/galah/galah.config"
+            )
+        except IOError:
+            pass
+
+    local_config = {}
+
+    # Note we make a **shallow** copy of the defaults here
+    user_config = dict(defaults)
+
+    # If the configuration didn't load correctly, just use the defaults
+    if loaded:
+        user_config.update(loaded.config)
+
+    # The prefix values we will look for when scanning the configuration file.
+    prefix = "%s/" % domain
+    global_prefix = "global/"
+
+    # Grab all the configuration values for the given domain, and any global
+    # configuration values.
+    for k, v in user_config.items():
+        if k.startswith(prefix) and k != len(prefix):
+            local_key = k[len(prefix):]
+
+            local_config[local_key] = v
+
+            # If the user specified domain specific handlers, attach them to the
+            # coorect logger. We know this will never be done multiple times
+            # because this function is memoized.
+            if local_key == "LOG_HANDLERS":
+                handlers = tuplify(v)
+
+                for i in handlers:
+                    logging.getLogger("galah." + domain).addHandler(i)
+        elif k.startswith(global_prefix) and k != len(global_prefix):
+            global_key = k[len(global_prefix):]
+
+            local_config[global_key] = v
+
+            # If the user specified global log handlers, attach them to the
+            # root logger.
+            if first_load and global_key == "LOG_HANDLERS":
+                handlers = tuplify(v)
+
+                for i in handlers:
+                    logging.getLogger("galah").addHandler(i)
+
+    return local_config
--- galah/web/__init__.py
+++ galah/web/__init__.py
@@ -2,23 +2,7 @@ from flask import Flask
 app = Flask("galah.web")
 
 from galah.base.config import load_config
-try:
-    app.config.update(load_config("/etc/galah/galah.config", "web"))
-except IOError as e:
-    exit(
-        "Could not load config file at /etc/galah/galah.config.\n\t" + str(e)
-    )
-
-if "LOG_HANDLERS" in app.config:
-    import logging
-    
-    # If a single handler was passed directly, convert it into a one-tuple
-    if isinstance(app.config["LOG_HANDLERS"], logging.Handler):
-        app.config["LOG_HANDLERS"] = (app.config["LOG_HANDLERS"], )
-        
-    # Add any handlers the user wants
-    for i in app.config["LOG_HANDLERS"]:
-        app.logger.addHandler(i)
+app.config.update(load_config("web"))
 
 oauth_enabled = bool(
     app.config.get("GOOGLE_SERVERSIDE_ID") and
