a756825d55794478119bce930cd629a83b6a5bad
John Sullivan <jsull003@ucr.edu>
2013-01-13 18:42:09 -0800

    Fixed lingering issue.
    * Seriously, it was about lingering.

--- galah/shepherd/api.py
+++ galah/shepherd/api.py
@@ -19,9 +19,10 @@
 import zmq
 
 context = zmq.Context()
-context.linger = 0
+context.linger = 2 * 1000
 
 def send_test_request(shepherd_host, submission_id):
+    # TODO: Make the socket thread-local.
     # Create a new socket to send a test request to shepherd.
     shepherd = context.socket(zmq.DEALER)
 
