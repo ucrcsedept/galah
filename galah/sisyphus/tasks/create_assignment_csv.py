c5ef412192ed05773e6e9cc8226a48884fa4300d
brownhead <jsull003@ucr.edu>
2013-02-14 17:21:16 -0800

    get_csv no longer balks on empty results.
    * Fixes #217.

--- galah/sisyphus/tasks/create_assignment_csv.py
+++ galah/sisyphus/tasks/create_assignment_csv.py
@@ -76,12 +76,12 @@ def _create_assignment_csv(csv_id, requester, assignment):
         csv_file = open(config["CSV_DIRECTORY"] + str(csv_id), "w")
 
         for i in submissions:
-            score = 0.0
+            score = "None"
             if i.test_results:
                 test_result = TestResult.objects.get(id = i.test_results)
-                score = test_result.score
+                score = str(test_result.score)
 
-            print >> csv_file, "%s,%f,%s" % \
+            print >> csv_file, "%s,%s,%s" % \
                 (i.user, score, i.timestamp.strftime("%Y-%m-%d-%H-%M-%S"))
 
         csv_file.close()
