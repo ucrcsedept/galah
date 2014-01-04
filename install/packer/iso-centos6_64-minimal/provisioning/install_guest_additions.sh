4084ccf11a63c5c3607ed45a75f06354f209d472
brownhead <jsull003@ucr.edu>
2014-01-03 17:41:34 -0800

    Guest additions correctly installed in iso config

--- install/packer/iso-centos6_64-minimal/provisioning/install_guest_additions.sh
+++ install/packer/iso-centos6_64-minimal/provisioning/install_guest_additions.sh
@@ -1,3 +1,11 @@
+# Expand variables and fail on first error
+set -e
+set -x
+
+# Install dependencies
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum -y install perl dkms gcc kernel-headers-$(uname -r) kernel-devel-$(uname -r)
+
 # Just in case guest additions writes anything to the current directory, we'll
 # change into /tmp
 cd /tmp
@@ -7,7 +15,7 @@ MOUNT_POINT=$(mktemp -d)
 mount -o loop /root/VBoxGuestAdditions.iso $MOUNT_POINT
 
 # Install guest additions
-bash $MOUNT_POINT/VBoxLinuxAdditions.run
+bash $MOUNT_POINT/VBoxLinuxAdditions.run || true
 
 # Unmount the ISO
 umount $MOUNT_POINT
