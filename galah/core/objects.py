c5a0a04a5bcce83b326a234be492cf39e2f5c7d1
brownhead <jsull003@ucr.edu>
2014-01-10 13:13:54 -0800

    Finished up NodeID class.
    * Resolves #404.

--- galah/core/objects.py
+++ galah/core/objects.py
@@ -1,43 +1,68 @@
-# stdlib
-import uuid
-
 # external
 from mangoengine import *
 
-from galah.base.config import load_config
-config = load_config("core")
-
-import logging
-log = logging.getLogger("galah.core.redis")
-
 class NodeID(Model):
+    """
+    A node is a single application such as a vmfactory or webapp process.
+
+    :ivar machine: A unicode string identifying the machine the node is running
+        on. The System Administrator can set this to whatever makes the most
+        sense to them (hostname, IP address, nickname ...).
+    :ivar local: An integer uniquely identifying the application on its
+        machine. This is part of the ID is automatically allocated.
+
+    """
+
     machine = UnicodeField()
-    """A unique ID for the machine the node is running on."""
+    local = IntegralField(bounds = (0, None))
 
-    local = UnicodeField()
-    """
-    A unique ID for the actual application. Only needs to be unique to the
-    machine.
+    def serialize(self):
+        """
+        Returns a string representation of this NodeID that can be passed to
+        deserialize() later.
 
-    """
+        .. note::
+
+            Validation is performed before serializing by calling the
+            validate() function.
+
+        :returns: A UTF-8 encoded string (not a ``unicode`` instance however).
+
+        """
+
+        # Our serialized string may not be capable of being deserialized if
+        # machine and local aren't valid so we validate inside the function.
+        self.validate()
+
+        return "%s:%d" % (self.machine.encode("utf_8"), self.local)
 
     @classmethod
-    def get_mine(cls):
-        """Returns the current node's NodeID."""
+    def deserialize(cls, string):
+        """
+        Returns the NodeID object serialized in string.
 
-        if hasattr(cls, "_saved_local_id"):
-            local = cls._saved_local_id
+        .. note::
+
+            The result is validated before being returned by calling the
+            validate() function.
+
+        :param string: A UTF-8 string as provided by serialize().
+
+        :returns: A NodeID instance.
+
+        """
+
+        for i in xrange(0, len(string)):
+            if string[len(string) - i - 1] == ":":
+                break
         else:
-            # We always generate a new ID rather than reading it from a file
-            cls._saved_local_id = local = uuid.uuid4()
+            raise ValueError("string %s is not a serialized NodeID" %
+                (repr(string), ))
 
-        if hasattr(cls, "_saved_machine_id"):
-            machine = cls._saved_machine_id
-        else:
-            # TODO: This should make a peristant ID on the filesystem.
-            cls._saved_machine_id = machine = uuid.uuid4()
+        local = int(string[len(string) - i:])
+        machine = string[:len(string) - i - 1].decode("utf_8")
 
-        return cls(machine = machine, local = local)
+        result = cls(machine = machine, local = local)
+        result.validate()
 
-    def __str__(self):
-        return "%s::%s" % (self.machine, self.local)
+        return result
