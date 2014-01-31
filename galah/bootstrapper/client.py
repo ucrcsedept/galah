409e96deafd338a484d3ac42cd7fff47338a45cd
brownhead <jsull003@ucr.edu>
2014-01-31 14:31:37 -0800

    Implemented part of boostrapper.
    * The bootstrapper server can now receive and decode messages
      correctly.
    * The boostrapper client module can serialize messages appropriately.

---
+++ galah/bootstrapper/client.py
@@ -0,0 +1,45 @@
+import StringIO
+
+BOOTSTRAPPER_PORT = 51749
+"""
+The port the bootstrapper will be listening on per the Boostrapper Protocol.
+
+.. note::
+
+    Do not change this value, it will not affect the port the bootstrapper is
+    listening on. This value is only meant to be a reference.
+
+"""
+
+COMMANDS = set(["ping"])
+"""The set of valid commands to send to the bootstrapper server."""
+
+def serialize_message(command, payload):
+    """
+    Serializes a message according to the Bootstrapper Protocol.
+
+    .. note::
+
+        payload must be of type str and will not be translated in any way.
+
+    """
+
+    buf = StringIO.StringIO()
+
+    buf.write(command.encode("ascii"))
+    buf.write(" ")
+
+    buf.write(str(len(payload)).encode("ascii"))
+    buf.write(" ")
+
+    if not isinstance(payload, str):
+        raise TypeError("payload must be a str object, got %s" % (
+            type(payload).__name__))
+    buf.write(payload)
+
+    return buf.getvalue()
+
+if __name__ == "__main__":
+    import socket
+    sock = socket.create_connection(("localhost", 51749))
+    sock.sendall(serialize_message("hello", "bob"))
--- galah/bootstrapper/server.py
+++ galah/bootstrapper/server.py
@@ -1,30 +1,150 @@
-class BaseState(object):
+# stdlib
+import socket
+import select
+import StringIO
 
+# The interface and port to listen on
+LISTEN_ON = ("", 51749)
 
-class Server(object):
-    states = set(u"NOT_READY", u"IDLE", u"HARNESS_READY", u"TEST_READY",
-       u"RUNNING_TEST", u"RESULTS_READY")
+class Message(object):
+    def __init__(self, command = None, num_bytes = None, payload = None):
+        self.command = command
+        self.num_bytes = num_bytes
+        self.payload = payload
 
-    commands = set(u"init")
+class Decoder(object):
+    """
+    An object capable of decoding messages sent down the wire according to the
+    Bootstrapper Protocol.
+
+    """
+
+    STATES = set(["COMMAND", "NUM_BYTES", "PAYLOAD"])
 
     def __init__(self):
-        self.state = states
+        self.state = "COMMAND"
+        self.msg = Message()
+        self._buffer_clear()
 
-    @property
-    def state(self):
-        return self._state
+    def _buffer_write(self, data):
+        self.buffer.write(data)
+        self.buffer_size += len(data)
 
-    @state.setter
-    def state(self, value):
-        assert isinstance(value, unicode) and value in states, \
-            "invalid state: %r" % (value, )
+    def _buffer_clear(self):
+        self.buffer = StringIO.StringIO()
+        self.buffer_size = 0
 
-        self._state = value
+    def decode(self, character):
+        """
+        Attempts to decode the current message using the supplied character.
 
-    def handle_command(self, command, payload):
-        func = getattr(self, "handle_%s" % (command.encode("ascii"), ), None)
-        if fun is None:
-            raise ValueError("command %r not found" % (command, ))
+        :returns: None if the current message is not yet complete yet so
+            cannot be fully decoded, or a Message object if the character was
+            the final byte in the message.
 
-    @command
-    def handle_
+        """
+
+        assert self.state in self.STATES
+        assert isinstance(character, str)
+        assert len(character) == 1
+
+        print repr(character), repr(self.state), repr(self.buffer.getvalue())
+
+        if self.state == "COMMAND":
+            if character == " ":
+                self.msg.command = self.buffer.getvalue().decode("ascii")
+
+                self.state = "NUM_BYTES"
+                self._buffer_clear()
+            else:
+                self._buffer_write(character)
+        elif self.state == "NUM_BYTES":
+            if character == " ":
+                decoded_data = self.buffer.getvalue().decode("ascii")
+                self.msg.num_bytes = int(decoded_data)
+
+                self.state = "PAYLOAD"
+                self._buffer_clear()
+            else:
+                self._buffer_write(character)
+        elif self.state == "PAYLOAD":
+            self._buffer_write(character)
+
+            assert self.buffer_size <= self.msg.num_bytes
+
+            if self.buffer_size == self.msg.num_bytes:
+                self.msg.payload = self.buffer.getvalue()
+
+                full_command = self.msg
+                self.msg = Message()
+
+                self._decoder_state = "COMMAND"
+                self._buffer_clear()
+
+                return full_command
+
+        return None
+
+class Connection(object):
+    """
+    :ivar sock: The ``socket`` instance returned by ``socket.accept()``.
+    :ivar address: The address of the node at the other end of the connection.
+
+    """
+
+    def __init__(self, sock, address):
+        self.sock = sock
+        self.address = address
+
+        self._decoder = Decoder()
+
+    # Necessary to support select.select()
+    def fileno(self):
+        return self.sock.fileno()
+
+    def read(self):
+        """
+        Reads in data waiting in the socket and returns a list of Message
+        objects that were decoded.
+
+        :returns: ``None``
+
+        """
+
+        messages = []
+        data = self.sock.recv(4096)
+        for i in data:
+            msg = self._decoder.decode(i)
+            if msg is not None:
+                messages.append(msg)
+        return messages
+
+def main():
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # This is useful during testing
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_sock.bind(LISTEN_ON)
+    server_sock.listen(2)
+
+    connections = []
+
+    while True:
+        # Block until one of the sockets we're looking at has data waiting to
+        # be read or connections waiting to be accepted.
+        socks, _w, _e = select.select([server_sock] + connections, [], [])
+
+        for sock in socks:
+            if sock is server_sock:
+                # Accept any new connections
+                sock, address = server_sock.accept()
+                connections.append(Connection(sock, address))
+
+                print "New connection from", address
+            else:
+                for msg in sock.read():
+                    print "Got message:", msg
+
+if __name__ == "__main__":
+    main()
