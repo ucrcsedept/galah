ffc5fb88e0a9007fb3f71b7bff9fb2e4f0125aa4
John Sullivan <josullivan@mozilla.com>
2014-08-19 18:05:34 -0700

    Relicensed under MIT.
    This was already done on the deserted rewrite branch but unfortunately
    a clean merge wasn't possible so I had to do it manually.

--- galah/base/prioritydict.py
+++ galah/base/prioritydict.py
@@ -1,21 +1,3 @@
-# Copyright 2012 John Sullivan
-# Copyright 2012 Other contributors as noted in the CONTRIBUTORS file
-#
-# This file is part of Galah.
-#
-# Galah is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Galah is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with Galah.  If not, see <http://www.gnu.org/licenses/>.
-
 # Snippet below derived from code published by Matteo Dell'Amico and published
 # under the unlicense.
 #     see: https://gist.github.com/4451520
