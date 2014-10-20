502b84ba139710c5989a08c84e9016b9a3019806
brownhead <johnsullivan.pem@gmail.com>
2014-10-20 13:52:19 -0700

    `run_server.py` now sets up debug logger.
    This should make debugging log configuration problems a little easier.
    
    Test Plan:
    Added debug log in main and ran `run_server.py` in vagrant box.
    Observed that log message spilled out. Ran `run_server.py --quiet` and
    observed that log message did not spill out.

--- galah/web/run_server.py
+++ galah/web/run_server.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 
+import logging
+import sys
+
+def setup_debug_logging():
+    logger_galah = logging.getLogger("galah")
+    logger_galah.setLevel(logging.DEBUG)
+
+    streamhandler = logging.StreamHandler()
+    streamhandler.setFormatter(logging.Formatter(
+        "[%(levelname)s;%(name)s;%(asctime)s]: %(message)s"
+    ))
+    logger_galah.addHandler(streamhandler)
 
 def main():
     from galah.web import app
     app.run()
 
 if __name__ == "__main__":
+    if "--quiet" not in sys.argv:
+        setup_debug_logging()
+
     main()
